2be869c04bf4a5adf1aba87348421480ddd0325d
gilbert <agbekponouv@gmail.com>
2024-09-15 00:37:08 -0400

    Authentification Modif du ficher env

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ require('dotenv').config();
     //because it give us starting point 
     const app = express();
     //mongodb string
-    const DB = "mongodb+srv://agbekponouv:Vignon1999@cluster0.lcotb.mongodb.net/"
+   
     //midlleware to register route or to mount route
     app.use(express.json());
    app.use(authRouter);
 
-    mongoose.connect(DB).then(()=>{
-    console.log('mongodb connected');
-   });
+   mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to Mongodb');
+})
     // start the server and specified the port 
     app.listen(PORT,"0.0.0.0",function(){
         //log the number 
